b80e4201e235b389ce622f92cce31ead55953296
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2022-12-16 23:42:12 +0000

    updates

--- terraform/atlantis/infrastructure/aws/ecs.tf
+++ terraform/atlantis/infrastructure/aws/ecs.tf
@@ -11,7 +11,8 @@ resource "aws_ecs_task_definition" "atlantis" {
   cpu                      = 256
   memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = jsonencode(
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode(
     [
       {
         name      = "atlantis"
